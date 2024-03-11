360ca6a515e82dcfd6393692e669ed05fdae4698
hamzacodzee <hamza.codezee@gmail.com>
2024-03-11 14:59:53 +0530

    reset,changecam,stop

--- src/Webcam.js
+++ src/Webcam.js
@@ -1,58 +1,49 @@
-import React, { useRef, useEffect, useCallback, useState } from "react";
+import React, { useRef, useCallback, useState } from "react";
 import Webcam from "react-webcam";
 
 const WebCam = () => {
   const webcamRef = useRef(null);
   const [image, setImage] = useState("");
-  const [permissionGranted, setPermissionGranted] = useState(false);
-  const [error, setError] = useState(null);
-
-  console.log("permissionGranted", permissionGranted, error);
-
-  const requestUserMedia = useCallback(async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-      });
-      if (webcamRef.current) {
-        webcamRef.current.video.srcObject = stream;
-        setPermissionGranted(true);
-      }
-    } catch (error) {
-      console.error("Error accessing user media:", error);
-      setError(error.message); // Set error message
-    }
-  }, []);
+  const [changeCameraMode, setChangeCameraMode] = useState(true);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     console.log(imageSrc);
     setImage(imageSrc);
+    const tracks = webcamRef.current.video.srcObject.getTracks();
+    tracks.forEach((track) => track.stop());
   }, [webcamRef]);
 
-  useEffect(() => {
-    requestUserMedia();
-  }, [requestUserMedia]);
+  const videoConstraints = {
+    width: 220,
+    height: 200,
+    facingMode: changeCameraMode ? "user" : "environment",
+  };
+
+  const reset = () => {
+    setChangeCameraMode(true);
+    setImage("");
+  };
 
   return (
     <div>
-      {/* {!permissionGranted && !error && (
-        <button onClick={requestUserMedia}>Grant Camera Access</button>
+      {!image && (
+        <>
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+
+          <button onClick={capture}>Capture photo</button>
+          <button onClick={() => setChangeCameraMode(!changeCameraMode)}>
+            Change Mode
+          </button>
+        </>
       )}
-      {error && <p>Error: {error}</p>} */}
-      {/* {permissionGranted && ( */}
-      <div>
-        <Webcam
-          audio={false}
-          // height={320}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          // width={280}
-        />
-        <button onClick={capture}>Capture photo</button>
-        {image && <img src={image} alt="Captured" />}
-      </div>
-      {/* )} */}
+      {image && <img src={image} alt="" />}
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
